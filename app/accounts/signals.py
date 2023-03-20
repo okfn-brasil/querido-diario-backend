34345b3da97b1f928c0ab420c1828040056adb92
Giulio Carvalho <giulioccavalcante@gmail.com>
2023-03-20 14:47:57 -0300

    Corrige endpoint de redefinição de senha

--- app/accounts/signals.py
+++ app/accounts/signals.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.dispatch import receiver
 from django_rest_passwordreset.signals import reset_password_token_created
 
@@ -8,19 +9,19 @@ from libs.utils.email import Email, send_email
 def password_reset_token_created(
     sender, instance, reset_password_token, *args, **kwargs
 ):
-    uri = "/redefinir-senha"
     key = reset_password_token.key
-    absolute = instance.request.build_absolute_uri(uri)
-    reset_password_url = f"{absolute}?token={key}"
+    reset_password_url = (
+        f"{settings.FRONT_BASE_URL}/educacao/redefinir-senha?token={key}"
+    )
 
     message = "Olá, \n"
     message += "Você pediu para redefinir sua senha, para fazê-lo basta clicar no link abaixo: \n"
     message += reset_password_url + " \n\n"
-    message += "Caso não tenha sido você que selecionou essa opção, por favor ignore este email.\n"
+    message += "Caso não tenha selecionado essa opção, por favor, ignore este e-mail.\n"
 
     send_email(
         Email(
-            subject="Redefinir senha para Querido Diário.",
+            subject="Querido Diário - Redefinição de senha",
             email_to=[reset_password_token.user.email],
             message=message,
         )
